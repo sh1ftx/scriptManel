d58f583d63e7b972f37cb9a84889dafaf9218f84
Vinycius <vinyciusalves155@gmail.com>
2026-01-31 16:21:28 -0300

    fix: corrigir erro de tabela nao encontrada e ajustes de identação

--- models/amigo.js
+++ models/amigo.js
@@ -1,19 +1,19 @@
 // Arquivo: models/amigo.js
-'use strict';
+"use strict";
 module.exports = (sequelize, DataTypes) => {
-const Amigo = sequelize.define(
-'Amigo',
-{
-nome: { type: DataTypes.STRING, allowNull: false },
-email: { type: DataTypes.STRING, allowNull: false }
-},
-{ tableName: 'Amigo' }
-);
-/*
+    const Amigo = sequelize.define(
+        "Amigo",
+        {
+            nome: { type: DataTypes.STRING, allowNull: false },
+            email: { type: DataTypes.STRING, allowNull: false },
+        },
+        { tableName: "Amigo" },
+    );
+    /*
 Amigo.associate = function(models) {
 Amigo.hasMany(models.Jogo, { foreignKey: 'amigoId', as: 'jogos' });
 Amigo.hasMany(models.Emprestimo, { foreignKey: 'amigoId', as: 'emprestimos' });
 };
 */
-return Amigo;
+    return Amigo;
 };
--- models/emprestimo.js
+++ models/emprestimo.js
@@ -1,19 +1,22 @@
 // Arquivo: models/emprestimo.js
-'use strict';
+"use strict";
 module.exports = (sequelize, DataTypes) => {
-const Emprestimo = sequelize.define(
-'Emprestimo',
-{
-jogoId: { type: DataTypes.INTEGER, allowNull: false },
-amigoId: { type: DataTypes.INTEGER, allowNull: false },
-dataInicio: { type: DataTypes.STRING, allowNull: false },
-dataFim: { type: DataTypes.STRING, allowNull: true }
-},
-{ tableName: 'Emprestimo' }
-);
-Emprestimo.associate = function(models) {
-Emprestimo.belongsTo(models.Jogo, { foreignKey: 'jogoId', as: 'jogo' });
-Emprestimo.belongsTo(models.Amigo, { foreignKey: 'amigoId', as: 'amigo' });
-};
-return Emprestimo;
+    const Emprestimo = sequelize.define(
+        "Emprestimo",
+        {
+            jogoId: { type: DataTypes.INTEGER, allowNull: false },
+            amigoId: { type: DataTypes.INTEGER, allowNull: false },
+            dataInicio: { type: DataTypes.STRING, allowNull: false },
+            dataFim: { type: DataTypes.STRING, allowNull: true },
+        },
+        { tableName: "Emprestimo" },
+    );
+    Emprestimo.associate = function (models) {
+        Emprestimo.belongsTo(models.Jogo, { foreignKey: "jogoId", as: "jogo" });
+        Emprestimo.belongsTo(models.Amigo, {
+            foreignKey: "amigoId",
+            as: "amigo",
+        });
+    };
+    return Emprestimo;
 };
