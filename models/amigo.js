3fba3a335914e429f522b4af4845fd627c81b6e2
MichasBaiano <programamichel@gmail.com>
2026-01-31 13:06:43 -0300

    ✨ Criando tabela Amigo

---
+++ models/amigo.js
@@ -0,0 +1,18 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  const Amigo = sequelize.define(
+    'Amigo',
+    {
+      nome: { type: DataTypes.STRING, allowNull: false },
+      email: { type: DataTypes.STRING, allowNull: false }
+    },
+    { tableName: 'Amigo' }
+  );
+  /*
+  Amigo.associate = function(models) {
+  Amigo.hasMany(models.Jogo, { foreignKey: 'amigoId', as: 'jogos' });
+  Amigo.hasMany(models.Emprestimo, { foreignKey: 'amigoId', as: 'emprestimos' });
+  };
+  */
+  return Amigo;
+};
