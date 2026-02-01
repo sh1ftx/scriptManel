dcd55a045f2abeb40fc1e092cf5454022f8a6b04
mr.sh1ft <sh1ft.developer144k@gmaol.com>
2026-01-31 23:50:38 -0300

    refactor(update_simple): Apenas alguns ajustes

--- migrations/20260131160357-create-amigo.js
+++ migrations/20260131160357-create-amigo.js
@@ -1,16 +1,35 @@
+// 20260131160357-create-amigo.js
 'use strict';
+
 module.exports = {
-  async up(queryInterface, Sequelize) {
-  await queryInterface.createTable('Amigo', {
-    id: { allowNull: false, autoIncrement: true, primaryKey: true, type:
-    Sequelize.INTEGER },
-    nome: { allowNull: false, type: Sequelize.STRING },
-    email: { allowNull: false, type: Sequelize.STRING },
-    createdAt: { allowNull: false, type: Sequelize.DATE },
-    updatedAt: { allowNull: false, type: Sequelize.DATE }
-  });
+  up: async (queryInterface, Sequelize) => {
+    return queryInterface.createTable('Amigo', {
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false
+      },
+      nome: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      email: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      createdAt: {
+        type: Sequelize.DATE,
+        allowNull: false
+      },
+      updatedAt: {
+        type: Sequelize.DATE,
+        allowNull: false
+      }
+    });
   },
-  async down(queryInterface, Sequelize) {
-  await queryInterface.dropTable('Amigo');
+
+  down: async (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('Amigo');
   }
 };
