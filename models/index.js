861a0ade83bc32bc7e4c94ae7938bd8ef92ca829
mr.sh1ft <sh1ft.developer144k@gmaol.com>
2026-01-31 23:29:42 -0300

    refactor(sequelize): reorganiza estrutura de models e seeds

--- models/index.js
+++ models/index.js
@@ -1,42 +1,56 @@
 'use strict';
 
-const fs = require('fs');
-const path = require('path');
+const fs        = require('fs');
+const path      = require('path');
 const Sequelize = require('sequelize');
-const process = require('process');
+
+const env      = process.env.NODE_ENV || 'development';
 const basename = path.basename(__filename);
-const env = process.env.NODE_ENV || 'development';
-const config = require(__dirname + '/../config/config.json')[env];
+const config   = require(path.join(__dirname, '../config/config.json'))[env];
+
 const db = {};
-
 let sequelize;
-if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
-} else {
-  sequelize = new Sequelize(config.database, config.username, config.password, config);
-}
 
-fs
-  .readdirSync(__dirname)
-  .filter(file => {
-    return (
-      file.indexOf('.') !== 0 &&
-      file !== basename &&
-      file.slice(-3) === '.js' &&
-      file.indexOf('.test.js') === -1
+/* ===========================
+   CONEXÃO COM O BANCO
+=========================== */
+sequelize = config.use_env_variable
+  ? new Sequelize(process.env[config.use_env_variable], config)
+  : new Sequelize(
+      config.database,
+      config.username,
+      config.password,
+      config
     );
-  })
-  .forEach(file => {
-    const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes);
+
+/* ===========================
+   CARREGAMENTO DOS MODELS
+=========================== */
+fs.readdirSync(__dirname)
+  .filter((file) =>
+    file !== basename &&
+    file.endsWith('.js') &&
+    !file.startsWith('.') &&
+    !file.endsWith('.test.js')
+  )
+  .forEach((file) => {
+    const modelPath = path.join(__dirname, file);
+    const model = require(modelPath)(sequelize, Sequelize.DataTypes);
     db[model.name] = model;
   });
 
-Object.keys(db).forEach(modelName => {
-  if (db[modelName].associate) {
-    db[modelName].associate(db);
+/* ===========================
+   ASSOCIAÇÕES
+=========================== */
+Object.values(db).forEach((model) => {
+  if (typeof model.associate === 'function') {
+    model.associate(db);
   }
 });
 
+/* ===========================
+   EXPORTAÇÕES
+=========================== */
 db.sequelize = sequelize;
 db.Sequelize = Sequelize;
 
