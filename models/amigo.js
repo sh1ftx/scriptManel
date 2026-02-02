f38104629e8390a0e6e27203236ba4b6d7b04591
Vinycius <vinyciusalves155@gmail.com>
2026-02-02 13:24:15 -0300

    feat(model/amigo): adicionar campo unique no email, auxiliando validacao de duplicatas

---
+++ models/amigo.js
@@ -0,0 +1,28 @@
+"use strict";
+module.exports = (sequelize, DataTypes) => {
+    const Amigo = sequelize.define(
+        "Amigo",
+        {
+            nome: {
+                type: DataTypes.STRING,
+                allowNull: false,
+            },
+            email: {
+                type: DataTypes.STRING,
+                allowNull: false,
+                unique: true,
+            },
+        },
+        { tableName: "Amigo" },
+    );
+
+    Amigo.associate = function (models) {
+        Amigo.hasMany(models.Jogo, { foreignKey: "amigoId", as: "jogos" });
+        Amigo.hasMany(models.Emprestimo, {
+            foreignKey: "amigoId",
+            as: "emprestimos",
+        });
+    };
+
+    return Amigo;
+};
